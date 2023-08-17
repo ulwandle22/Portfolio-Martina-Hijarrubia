74883714e89d89afa32398e8e357f6f4933cd4c6
ulwandle22 <martina.hijarrubia@gmail.com>
2023-08-17 18:36:25 -0300

    Project

---
+++ PracticaPortfolio/src/backend/MartinaHijarrubia/src/main/java/com/Portfolio/MartinaHijarrubia/MartinaHijarrubiaApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.MartinaHijarrubia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartinaHijarrubiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartinaHijarrubiaApplication.class, args);
+	}
+
+}
